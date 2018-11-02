95ddc1964e7e41ed47275c49a49121f3b082edf8
David Esteves <david@esteves.eu>
2018-11-02 19:12:06 +0400

    Various improvements and fixes

--- README.md
+++ README.md
@@ -4,26 +4,53 @@ Tested on Ubuntu 18.04
 nginx >= 1.5.9
 letsecncrypt >= 0.25
 
-Meant to be ran on a fresh server
+Meant to be ran on a fresh server.
+It will install/configure nginx and letsencrypt
+Use with caution if you already have nginx installed and configured.
+`/etc/nginx/sites-{available,enabled}/default` will be deleted.
+`/etc/nginx/sites-{available,enabled}/example.com` will be created.
+
+## DNS setup
+DNS records for example.com and www.example.com should already exist.
+It'll attempt to set a redirection from www.example.com to example.com, if there's no record for www.example.com it'll only do the setup for example.com
+
+### CAA
+To get a 100% rating a CAA record is needed.
+This is not necessary to do the setup and can be done later on.
 
 ## Installation
+### Get the code
 ```shell
-cd ~/
-git clone https://github.com/estevesd/ssl_setup.git .
-cd $_
-bash install.sh email@example.com
+git clone https://github.com/estevesd/ssl_setup.git ~/ssl_setup
 ```
+
+### Interactive Installation
+# specifying an email is necessary to register on letsencrypt. if nothing is specified here letsencrypt registration will ask for it anyway.
+```
+bash ~/ssl_setup/install.sh
+```
+### Unattended Installation
+`bash ~/ssl_setup/install.sh email@example.com [dh|no_dh] [nginx_tweak|no_nginx_tweak] [firewall_conf|no_firewall_conf]`
+1. An email is necessary to register on letsencrypt.
+2. `dh` or `no_dh` to generate a strong Diffie Hellman key or not (necessary to get 100% on Key Exchange)
+3. `nginx_tweak` or `no_nginx_tweak` to tweak nginx configuration or not (enables gzip)
+4. `firewall_conf` or `no_firewall_conf` to open ports using `ufw` (opens 22, 80 and 443)
 
 ## Usage
 `ssl_setup [domain] [path to root](optional, default:/var/www/[domain])`
-```shell
-ssl_setup example.com /var/http
-```
+This will create nginx configuration and SSL certificates.
 
 ## DH parameters
 Generating a 4096bits needed to get a 100% rating on Key exchange can take a very long time. So it's optional during installation.
 To generate it later on, run 'ssl_dhparam'
+Since it can take a very long time to generate a key, you can do it an another machine :
+`openssl dhparam -out my_dhparams.pem 4096`
+and then run
+`ssl_dhparam my_dhparams.pem`
 
 ## Renewal
 A cronjob will be added to check/renew certificates periodically
 `ssl_renew`
+
+## Test
+https://www.ssllabs.com/ssltest/analyze.html?d=example.com&latest=yes
--- install.sh
+++ install.sh
@@ -4,9 +4,15 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
+cp ssl_{setup,renew,dhparam} /usr/local/bin
+chmod 755 /usr/local/bin/ssl_{setup,renew,dhparam}
+cp -r var /
+
 add-apt-repository --yes --no-update ppa:certbot/certbot
 apt update
 apt install -y python-certbot-nginx nginx haveged
+
+# configure letsencrypt
 if [ -z "$1" ]; then
   letsencrypt register --agree-tos --no-eff-email
 else
@@ -17,23 +23,49 @@ cat >>/etc/letsencrypt/cli.ini <<EOF
 # Use a 4096 bit RSA key instead of 2048
 rsa-key-size = 4096
 EOF
+cp /etc/letsencrypt/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf.bkp
+cp /var/lib/ssl_setup/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf
 
-cp ssl_{setup,renew,dhparam} /usr/local/bin
-chmod 755 /usr/local/bin/ssl_{setup,renew,dhparam}
-cp -r var /
-read -p "Do you want to generate a Diffie Hellman? " -n 1 -r
-echo # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  ssl_dhparam
+# enable gzip in nginx
+if [ -z "$1" ]; then
+  read -p "Do you want to tweak nginx configuration (enables gzip)? [Y|N]" -n 1 -r
 else
-  echo "You can still generate a Diffie Hellman later on by runnning : ssl_dhparam"
+  if [ "$4" == "nginx_tweak" ]; then
+    REPLY="Y"
+  fi
+fi
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  grep "gzip_disable \"msie6\"" /etc/nginx/nginx.conf >>/dev/null || sed -i -e "s/gzip on;/gzip on;\n\tgzip_disable \"msie6\";/" /etc/nginx/nginx.conf
+  sed -i "s/# gzip/gzip/" /etc/nginx/nginx.conf
 fi
 
-grep "gzip_disable \"msie6\"" /etc/nginx/nginx.conf >>/dev/null || sed -i -e "s/gzip on;/gzip on;\n\tgzip_disable \"msie6\";/" /etc/nginx/nginx.conf
-sed -i "s/# gzip/gzip/" /etc/nginx/nginx.conf
+# Configure firewall
+if [ -z "$1" ]; then
+  read -p "Do you want to configure the firewall (opens 22, 80 and 443)? [Y|N]" -n 1 -r
+else
+  if [ "$4" == "firewall_conf" ]; then
+    REPLY="Y"
+  fi
+fi
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  which ufw || apt install -y ufw
+  ufw allow 'OpenSSH'
+  ufw allow 'Nginx Full'
+  ufw delete allow 'Nginx HTTP'
+  echo "y" | ufw enable
+fi
 
-which ufw || apt install -y ufw
-ufw allow 'OpenSSH'
-ufw allow 'Nginx Full'
-ufw delete allow 'Nginx HTTP'
-echo "y" | ufw enable
+# Generate strong Diffie Hellman
+if [ -z "$1" ]; then
+  read -p "Do you want to generate a Diffie Hellman? [Y|N]" -n 1 -r
+else
+  if [ -n "$2" ] && [ "$2" == "dh" ]; then
+    REPLY="Y"
+  fi
+fi
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "if it gets too long, you can cancel (Ctrl+c), and generate later with : ssl_dhparam"
+  ssl_dhparam
+else
+  echo "You can still generate a Diffie Hellman later on by runnning : ssl_dhparam or ssl_dhparam my_dhparam.pem"
+fi
